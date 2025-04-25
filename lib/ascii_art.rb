ec397cb0ea19c9c70e10b3283677cc37b111acf7
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-04-25 13:29:41 -0700

    Convert pixels averages to ASCII character and print

--- lib/ascii_art.rb
+++ lib/ascii_art.rb
@@ -6,6 +6,8 @@ require 'mini_magick'
 module AsciiArt
   class Error < StandardError; end
 
+  ASCII_BRIGHTNESS = '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+
   image = MiniMagick::Image.open('images/ascii-pineapple.jpg')
 
   length, width = image.dimensions
@@ -13,11 +15,13 @@ module AsciiArt
   puts 'Successfully constructed pixel matrix!'
   puts "Pixel matrix size: #{length} x #{width}"
 
-  puts 'Iterating through pixel brightness'
+  puts 'Iterating through pixel ASCII characters'
   image.get_pixels.each do |group|
     group.each do |pixel|
       pixel_average = pixel.sum / pixel.length
-      p pixel_average
+      puts pixel_average
+      divisor = (255 / ASCII_BRIGHTNESS.length)
+      p ASCII_BRIGHTNESS[(pixel_average / divisor).round]
     end
   end
 end
