ea78b7e2c5cc8676613704731d347f3dbf9e3e0a
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-04-25 15:15:55 -0700

    Set up three methods for converting RGB values to pixel brightness and display image

--- lib/ascii_art.rb
+++ lib/ascii_art.rb
@@ -3,25 +3,39 @@
 require_relative 'ascii_art/version'
 require 'mini_magick'
 
+def luminosity(pixel)
+  (0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2]) / 3
+end
+
+def lightness(pixel)
+  (pixel.max + pixel.min) / 2
+end
+
+def average(pixel)
+  pixel.sum / 3
+end
+
 module AsciiArt
   class Error < StandardError; end
 
-  ASCII_BRIGHTNESS = '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+  ASCII_BRIGHTNESS = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 
-  image = MiniMagick::Image.open('images/ascii-pineapple.jpg')
+  image = MiniMagick::Image.open('images/jason.jpg')
+  image.resize '500x375'
 
   length, width = image.dimensions
 
   puts 'Successfully constructed pixel matrix!'
   puts "Pixel matrix size: #{length} x #{width}"
 
-  puts 'Iterating through pixel ASCII characters'
+  puts 'Building ASCII image..'
+  divisor = (255.0 / ASCII_BRIGHTNESS.length)
   image.get_pixels.each do |group|
-    group.each do |pixel|
-      pixel_average = pixel.sum / pixel.length
-      puts pixel_average
-      divisor = (255 / ASCII_BRIGHTNESS.length)
-      p ASCII_BRIGHTNESS[(pixel_average / divisor).round]
+    row = group.map do |pixel|
+      pixel_average = luminosity(pixel)
+      ascii = ASCII_BRIGHTNESS[(pixel_average / divisor).round - 1]
+      ascii * 3
     end
+    puts row.join
   end
 end
