2eded2709dfc5cfaac35d38c7a219767725ae1f6
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-04-25 12:51:26 -0700

    Update gitignore and open image to print dimensions

--- lib/ascii_art.rb
+++ lib/ascii_art.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'ascii_art/version'
+require 'mini_magick'
 
 module AsciiArt
   class Error < StandardError; end
+
+  image = MiniMagick::Image.open('images/ascii-pineapple.jpg')
+
+  length, width = image.dimensions
+
+  puts 'Successfully loaded image!'
+  puts "Image size: #{length} x #{width}"
 end
