a26dadaa64d80cbe95c332ae4f6194c8b713b16b
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-04-29 08:40:23 -0700

    Add cli functionality

--- lib/ascii_art.rb
+++ lib/ascii_art.rb
@@ -2,6 +2,7 @@
 
 require_relative 'ascii_art/version'
 require 'mini_magick'
+require 'optparse'
 
 def luminosity(pixel)
   (0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2]) / 3
@@ -18,9 +19,30 @@ end
 module AsciiArt
   class Error < StandardError; end
 
+  options = {}
+
+  parser = OptionParser.new do |parser|
+    parser.banner = "Usage: ruby #{File.basename(__FILE__)} [options] <path_to_file>"
+
+    parser.on('--help', 'Display this help') do
+      puts parser
+      exit
+    end
+    parser.on('-p', '--path PATH', String, 'Path to the file') do |path|
+      options[:path] = path
+    end
+  end.parse!
+
+  if options[:path].nil?
+    puts 'Error: No file path provided.'
+    puts parser
+    exit
+  end
+  puts "Processing file: #{options[:path]}"
+
   ASCII_BRIGHTNESS = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'.reverse
 
-  image = MiniMagick::Image.open('images/caravaggio.jpg')
+  image = MiniMagick::Image.open(options[:path])
   image.resize '1699'
 
   length, width = image.dimensions
